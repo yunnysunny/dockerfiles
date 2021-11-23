6db2ca88234c606ed6df20e9ee267f074ff1b787
yunnysunny <yunnysunny@gmail.com>
2021-11-23 12:15:53 +0800

    修正jmeter构建脚本错误

--- centos/build_jmeter.sh
+++ centos/build_jmeter.sh
@@ -3,7 +3,7 @@ set -e
 
 TAG_LATEST=registry.cn-hangzhou.aliyuncs.com/whyun/base:jmeter-latest
 JMETER_VERSION=5.4.1
-docker pull registry.cn-hangzhou.aliyuncs.com/whyun/base:java-latest
+docker pull registry.cn-hangzhou.aliyuncs.com/whyun/base:jdk11-latest
 docker build . -f ./java.Dockerfile -t ${TAG_LATEST} --progress=plain  --build-arg JMETER_VERSION=${JMETER_VERSION}
 if [ "$NEED_PUSH" = "1" ] ; then
     docker push ${TAG_LATEST}
