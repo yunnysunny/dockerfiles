6dac87bbf744f38a17176210247371132d4be5d4
yunnysunny <yunnysunny@gmail.com>
2021-11-23 18:43:38 +0800

    将jmeter单独抽离到一个文件夹中

--- jmeter/build_jmeter.sh
+++ jmeter/build_jmeter.sh
@@ -4,7 +4,7 @@ set -e
 TAG_LATEST=registry.cn-hangzhou.aliyuncs.com/whyun/base:jmeter-latest
 JMETER_VERSION=5.4.1
 docker pull registry.cn-hangzhou.aliyuncs.com/whyun/base:jdk11-latest
-docker build . -f ./java.Dockerfile -t ${TAG_LATEST} --progress=plain  --build-arg JMETER_VERSION=${JMETER_VERSION}
+docker build ./docker -f ./jmeter.Dockerfile -t ${TAG_LATEST} --progress=plain  --build-arg JMETER_VERSION=${JMETER_VERSION}
 if [ "$NEED_PUSH" = "1" ] ; then
     docker push ${TAG_LATEST}
 fi
--- jmeter/jmeter.Dockerfile
+++ jmeter/jmeter.Dockerfile
@@ -5,5 +5,5 @@ ARG JMETER_VERSION
 RUN wget https://mirrors.tuna.tsinghua.edu.cn/apache/jmeter/binaries/apache-jmeter-${JMETER_VERSION}.tgz && \
     tar -zxvf apache-jmeter-${JMETER_VERSION}.tgz && \
     rm -rf apache-jmeter-${JMETER_VERSION}.tgz
-
-CMD while true; do sleep 1; done
+COPY . /
+ENTRYPOINT [ "/entrypoint.sh" ]
