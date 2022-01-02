2f2ce2a74efa0e4fdfcae5c5584bc90ccf044512
yunnysunny <yunnysunny@gmail.com>
2022-01-02 22:21:50 +0800

    增加github action

--- node-bench/build.sh
+++ node-bench/build.sh
@@ -3,12 +3,22 @@ set -e
 VERSION=0.2.0
 TAG_ALI_LATEST=registry.cn-hangzhou.aliyuncs.com/whyun/base:node-bench-latest
 TAG_OFFICAL_LATEST=yunnysunny/node-bench
-TAG_VERSION=registry.cn-hangzhou.aliyuncs.com/whyun/base:node-bench-${VERSION}
+TAG_ALI_VERSION=registry.cn-hangzhou.aliyuncs.com/whyun/base:node-bench-${VERSION}
+TAG_OFFICAL_VERSION=yunnysunny/node-bench:${VERSION}
 
 docker pull registry.cn-hangzhou.aliyuncs.com/whyun/base:alinode-latest
-docker build ./docker -f ./bench.Dockerfile -t ${TAG_ALI_LATEST} -t ${TAG_OFFICAL_LATEST} -t ${TAG_VERSION} --progress=plain
+docker build ./docker -f ./bench.Dockerfile \
+    -t ${TAG_ALI_LATEST} \
+    -t ${TAG_OFFICAL_LATEST} \
+    -t ${TAG_ALI_VERSION} \
+    -T ${TAG_OFFICAL_VERSION} \
+    --progress=plain
 if [ "$NEED_PUSH" = "1" ] ; then
     docker push ${TAG_ALI_LATEST}
-    if [ "$PUSH_TO_OFFICAL" = "1" ] ; then docker push $TAG_OFFICAL_LATEST fi
-    docker push ${TAG_VERSION}
+    
+    docker push ${TAG_ALI_VERSION}
+    if [ "$PUSH_TO_OFFICAL" = "1" ] ; then
+        docker push $TAG_OFFICAL_LATEST
+        docker push ${TAG_OFFICAL_VERSION}
+    fi
 fi
--- supervisor/build.sh
+++ supervisor/build.sh
@@ -2,17 +2,23 @@
 set -e
 
 TAG_ALI_LATEST_CENTOS=registry.cn-hangzhou.aliyuncs.com/whyun/base:supervisor-latest
+TAG_OFFICAL_LATEST_CENTOS=yunnysunny/supervisor
 TAG_ALI_LATEST_UBUNTU=registry.cn-hangzhou.aliyuncs.com/whyun/base:supervisor-ubuntu-latest
+TAG_OFFICAL_LATEST=unnysunny/supervisor-ubuntu
 CENTOS_IMAGE=centos:7
 UBUNTU_IMAGE=ubuntu
 docker pull ${CENTOS_IMAGE}
-docker build . -f ./Dockerfile -t ${TAG_ALI_LATEST_CENTOS} \
+docker build . -f ./Dockerfile \
+    -t ${TAG_ALI_LATEST_CENTOS} \
+    -t ${TAG_OFFICAL_LATEST_CENTOS} \
     --build-arg IMAGE_NAME=${CENTOS_IMAGE} \
     --build-arg IMAGE_TYPE=centos \
     --build-arg CRON_START_OPTION=n \
     --progress=plain
 docker pull ${UBUNTU_IMAGE}
-docker build . -f ./Dockerfile -t ${TAG_ALI_LATEST_UBUNTU} \
+docker build . -f ./Dockerfile \
+    -t ${TAG_ALI_LATEST_UBUNTU} \
+    -t ${TAG_OFFICAL_LATEST} \
     --build-arg IMAGE_NAME=${UBUNTU_IMAGE} \
     --build-arg IMAGE_TYPE=ubuntu \
     --build-arg CRON_START_OPTION=f \
@@ -20,4 +26,8 @@ docker build . -f ./Dockerfile -t ${TAG_ALI_LATEST_UBUNTU} \
 if [ "$NEED_PUSH" = "1" ] ; then
     docker push ${TAG_ALI_LATEST_CENTOS}
     docker push ${TAG_ALI_LATEST_UBUNTU}
+    if [ "$PUSH_TO_OFFICAL" = "1" ] ; then
+        docker push $TAG_OFFICAL_LATEST
+        docker push ${TAG_OFFICAL_VERSION}
+    fi
 fi
