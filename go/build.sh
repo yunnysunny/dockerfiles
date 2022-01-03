0d8db35a6cc66fcb6d77066691a1fa079f2cf7c7
yunnysunny <yunnysunny@gmail.com>
2022-01-03 11:29:26 +0800

    修正构建脚本错误

--- go/build.sh
+++ go/build.sh
@@ -5,22 +5,23 @@ GO_VERSION=1.17.2
 GOPRIVATE=gitlab.com
 TAG_ALI_LATEST=registry.cn-hangzhou.aliyuncs.com/whyun/base:golang-latest
 TAG_OFFICAL_LATEST=yunnysunny/golang
-TAG_CURRENT=registry.cn-hangzhou.aliyuncs.com/whyun/base:golang-${GO_VERSION}
+TAG_ALI_CURRENT=registry.cn-hangzhou.aliyuncs.com/whyun/base:golang-${GO_VERSION}
 SAVE_NAME=go
 . "$(dirname "$PWD")/util/docker_cache.sh"
 load_cache $SAVE_NAME
 
 docker pull centos:7
-docker build . -f ./Dockerfile -t ${TAG_ALI_LATEST} -t ${TAG_OFFICAL_LATEST} -t ${TAG_CURRENT} \
+docker build . -f ./Dockerfile -t ${TAG_ALI_LATEST} -t ${TAG_OFFICAL_LATEST} -t ${TAG_ALI_CURRENT} \
     --build-arg GO_VERSION=${GO_VERSION} --build-arg GOPRIVATE=${GOPRIVATE}
-save_cache $SAVE_NAME ${TAG_ALI_LATEST} ${TAG_OFFICAL_LATEST}
 
 if [ "$NEED_PUSH" = "1" ] ; then
     info_print "push to ${TAG_ALI_LATEST}"
     docker push ${TAG_ALI_LATEST}
+    info_print "push to ${TAG_ALI_CURRENT}"
+    docker push ${TAG_ALI_CURRENT}
     if [ "$PUSH_TO_OFFICAL" = "1" ] ; then 
         info_print "push to ${TAG_OFFICAL_LATEST}";
         docker push ${TAG_OFFICAL_LATEST};    
     fi
-    docker push ${TAG_CURRENT}
 fi
+save_cache $SAVE_NAME ${TAG_ALI_LATEST} ${TAG_OFFICAL_LATEST}
