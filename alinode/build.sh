8e168bc7548fc7cb2ceb2751c87e9e659fb597c1
yunnysunny <yunnysunny@gmail.com>
2022-01-23 16:09:26 +0800

    使用原生docker的缓存功能

--- alinode/build.sh
+++ alinode/build.sh
@@ -8,7 +8,9 @@ TAG_VERSION=registry.cn-hangzhou.aliyuncs.com/whyun/base:alinode-${ALINODE_VERSI
 TAG_OFFICAL_VERSION=yunnysunny/alinode:${ALINODE_VERSION}
 
 SAVE_NAME=alinode
-CACHE_FILE=$CACHE_FROM_DIR/$SAVE_NAME
+# CACHE_FILE=$CACHE_FROM_DIR/$SAVE_NAME
+. "$(dirname "$PWD")/util/docker_cache.sh"
+load_cache $SAVE_NAME
 
 docker pull registry.cn-hangzhou.aliyuncs.com/whyun/base:centos-latest
 BUILD_PARAMS="./docker -f ./alinode.Dockerfile -t ${TAG_ALI_LATEST} \
@@ -16,17 +18,18 @@ BUILD_PARAMS="./docker -f ./alinode.Dockerfile -t ${TAG_ALI_LATEST} \
   -t ${TAG_OFFICAL_VERSION}
   --progress=plain \
   --build-arg ALINODE_VERSION=${ALINODE_VERSION}"
-if [ -d "$CACHE_FROM_DIR" ] ; then
-  BUILD_PARAMS="${BUILD_PARAMS}  --cache-from type=local,src=${CACHE_FILE} \
-  --cache-to type=local,mode=max,dest=${CACHE_FILE}"
-fi
-docker buildx build $BUILD_PARAMS
+# if [ -d "$CACHE_FROM_DIR" ] ; then
+#   BUILD_PARAMS="${BUILD_PARAMS}  --cache-from type=local,src=${CACHE_FILE} \
+#   --cache-to type=local,mode=max,dest=${CACHE_FILE}"
+# fi
+docker build $BUILD_PARAMS
 if [ "$NEED_PUSH" = "1" ] ; then
-    docker push --all-tags ${TAG_ALI_LATEST}
-    docker push  --all-tags ${TAG_VERSION}
+    docker push ${TAG_ALI_LATEST}
+    docker push ${TAG_VERSION}
     if [ "$PUSH_TO_OFFICAL" = "1" ] ; then 
         info_print "push to ${TAG_OFFICAL_LATEST}"
-        docker push  --all-tags ${TAG_OFFICAL_LATEST}
-        docker push  --all-tags ${TAG_OFFICAL_VERSION}
+        docker push  ${TAG_OFFICAL_LATEST}
+        docker push  ${TAG_OFFICAL_VERSION}
     fi    
 fi
+save_cache $SAVE_NAME ${TAG_ALI_LATEST} ${TAG_OFFICAL_LATEST} ${TAG_OFFICAL_VERSION} ${TAG_VERSION}
