ee9b5bd0e577d23e540de491fc9bf4e72c12d632
yunnysunny <yunnysunny@gmail.com>
2022-01-23 15:08:51 +0800

    将每个docker的构建拆分到单独的github action中

---
+++ builder/create_workflow.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BASE_SRC_FOLDER=(\
+    "alinode" "centos" "go" "hello" "jmeter" \
+    "node-bench" "supervisor" "ubuntu"\
+)
+CUR_PATH=$(dirname "$PWD")
+
+for dir in "${BASE_SRC_FOLDER[@]}"; do
+    export IMAGE_NAME=$dir
+    DIST_YML=${CUR_PATH}/.github/workflows/publish-${dir}-image.yml
+    eval "cat <<EOF
+$(< publish-image-header.yml)
+EOF
+    "  > "$DIST_YML"
+    cat publish-image-body.yml >> "$DIST_YML"
+done
